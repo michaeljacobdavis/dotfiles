e303a88807d260bdb063c4d98019faae9213dcb0
Michael Davis <michael.jacob.davis@gmail.com>
2015-10-10 19:42:10 -0700

    Update osx.sh

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
-#Show the ~/Library folder
-chflags nohidden ~/Library
+# System Settings
+# ===============
 
-# Disable the sound effects on boot
-sudo nvram SystemAudioVolume=" "
+# Disable ext change warning
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# Disable transparency in the menu bar and elsewhere on Yosemite
-defaults write com.apple.universalaccess reduceTransparency -bool true
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Disable disk image verification
+defaults write com.apple.frameworks.diskimages skip-verify -bool true && \
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true && \
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+# Keyboard
+# ========
 
 # Enable character repeat on keydown
 defaults write -g ApplePressAndHoldEnabled -bool false
@@ -18,24 +29,23 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 12
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 0
 
+# Look and Feel
+# =============
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Disable transparency in the menu bar and elsewhere on Yosemite
+defaults write com.apple.universalaccess reduceTransparency -bool true
+
 # Disable window animations ("new window" scale effect)
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 
-# Use plain text mode for new TextEdit documents
-defaults write com.apple.TextEdit RichText -int 0
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0.1
 
-# Set default Finder location to home folder (~/)
-defaults write com.apple.finder NewWindowTarget -string "PfLo" && \
-defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
-
-# Expand save panel by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-
-# Disable ext change warning
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-# Check for software updates daily, not just once per week
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+# Finder
+# ======
 
 # Use current directory as default search scope in Finder
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
@@ -46,25 +56,30 @@ defaults write com.apple.finder ShowPathbar -bool true
 # Show Status bar in Finder
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# Avoid creating .DS_Store files on network volumes
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-
-# Disable disk image verification
-defaults write com.apple.frameworks.diskimages skip-verify -bool true && \
-defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true && \
-defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+# Set default Finder location to home folder (~/)
+defaults write com.apple.finder NewWindowTarget -string "PfLo" && \
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}"
 
 # Show the ~/Library folder
 chflags nohidden ~/Library
 
-# Show absolute path in finder's title bar.
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
-
-# Auto-play videos when opened with QuickTime Player
-defaults write com.apple.QuickTimePlayerX MGPlayMovieOnOpen 1
+# Trash
+# =====
 
 # Disable the warning before emptying the Trash
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
 # Empty Trash securely by default
 defaults write com.apple.finder EmptyTrashSecurely -bool true
+
+# App Configurations
+# ==================
+
+# Use plain text mode for new TextEdit documents
+defaults write com.apple.TextEdit RichText -int 0
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Auto-play videos when opened with QuickTime Player
+defaults write com.apple.QuickTimePlayerX MGPlayMovieOnOpen 1
