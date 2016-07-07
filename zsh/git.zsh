ff87c104569f415ea326a230afc5d565abbb2915
Mike Davis <mike@michaeljacobdavis.com>
2016-07-07 10:12:34 -0700

    Clean slate

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -5,3 +5,6 @@ __git_files () {
 
 # Remove all merged branches
 alias git-clean="git branch --merged | grep -v '\*' | xargs -n 1 git branch -d"
+
+# Removes all branches other than master
+alias git-clean-slate="git branch | grep -v 'master' | xargs git branch -D"
